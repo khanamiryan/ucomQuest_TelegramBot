3eec8977b4e51f5e391bb2f4abb6d24bd1a0af48
Narek Tutikyan <Narek@seesawlabs.com>
2021-08-16 17:54:19 +0400

    removed unused lines

--- index.js
+++ index.js
@@ -7,15 +7,6 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.use(function (req, res, next) {
-  console.log('Time:', Date.now(), res);
-  let method = req.method;
-  let url = req.url;
-  // let status = res.statusCode;
-  next();
-});
-
-
 app.use("/", appRoute)
 
 app.listen(process.env.port,  () => {
