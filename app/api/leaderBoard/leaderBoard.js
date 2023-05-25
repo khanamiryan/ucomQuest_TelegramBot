04c5f97aa41f8edbfdd5c7511c7096cd87623724
akhanamiryan <ashot.khanamiryan@volo.global>
2023-05-26 00:27:21 +0400

    fix: added sorting

--- app/api/leaderBoard/leaderBoard.js
+++ app/api/leaderBoard/leaderBoard.js
@@ -5,7 +5,7 @@ const router = express.Router()
 router.get('/', async (req, res) => {
     try {
         const users = await Users.find({ role: 'player', telegramId: { $exists: true } })
-            .sort({ total: -1 });
+            .sort({ allPoint: -1 });
 
         const percent = await Users.aggregate([
             {
@@ -31,61 +31,61 @@ router.get('/', async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 });
-router.get('/', async (req, res) => {
-  const aggregate = [
-    {
-      $match: {
-        role: 'player',
-        telegramId: { $exists: true}
-      }
-    },
-    {
-      $lookup:
-        {
-          from: "users",
-          localField: "adminId",
-          foreignField: "_id",
-          as: "admin"
-        }
-    },
-    {
-      $addFields: {adminData: { $arrayElemAt: [ "$admin", 0 ] }}
-    },
-    {$project: {admin: 0}},
-    {
-      $lookup:
-        {
-          from: "locations",
-          localField: "playingLocationId",
-          foreignField: "_id",
-          as: "location"
-        }
-    },
-    {
-      $addFields: {locationData: { $arrayElemAt: [ "$location", 0 ] }}
-    },
-      {
-          $addFields: {locationName: "$locationData.name" },
-      },
-      {$project: {location: 0}},
-      // { $addFields: {  total: { $add: [ "$locationPoint", "$allPoint" ] }} },
-      { $sort : { total : -1 } },
-  ];
-  const percent = await Users.aggregate([...aggregate,     {
-    $group : {
-      _id: null,
-      max: { $max : "$total" },
-      // min: { $min : "$total" }
-    },
-  }])
-  const users = await Users.aggregate([...aggregate,
-    {
-      $addFields: {
-        percent: 77 / (percent && percent[0] && percent[0].max) || 1
-      }
-    }])
-  res.json(users)
-})
+// router.get('/', async (req, res) => {
+//   const aggregate = [
+//     {
+//       $match: {
+//         role: 'player',
+//         telegramId: { $exists: true}
+//       }
+//     },
+//     {
+//       $lookup:
+//         {
+//           from: "users",
+//           localField: "adminId",
+//           foreignField: "_id",
+//           as: "admin"
+//         }
+//     },
+//     {
+//       $addFields: {adminData: { $arrayElemAt: [ "$admin", 0 ] }}
+//     },
+//     {$project: {admin: 0}},
+//     {
+//       $lookup:
+//         {
+//           from: "locations",
+//           localField: "playingLocationId",
+//           foreignField: "_id",
+//           as: "location"
+//         }
+//     },
+//     {
+//       $addFields: {locationData: { $arrayElemAt: [ "$location", 0 ] }}
+//     },
+//       {
+//           $addFields: {locationName: "$locationData.name" },
+//       },
+//       {$project: {location: 0}},
+//       // { $addFields: {  total: { $add: [ "$locationPoint", "$allPoint" ] }} },
+//       { $sort : { total : -1 } },
+//   ];
+//   const percent = await Users.aggregate([...aggregate,     {
+//     $group : {
+//       _id: null,
+//       max: { $max : "$total" },
+//       // min: { $min : "$total" }
+//     },
+//   }])
+//   const users = await Users.aggregate([...aggregate,
+//     {
+//       $addFields: {
+//         percent: 77 / (percent && percent[0] && percent[0].max) || 1
+//       }
+//     }])
+//   res.json(users)
+// })
 router.get('/admins', (req, res) => {
   Users.find({role: 'admin'}).sort({role: -1}).then(r => {
     res.json(r)
