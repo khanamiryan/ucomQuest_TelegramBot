5037227657a3f1f141a13ef7c9195e763b4e0adf
akhanamiryan <ashot.khanamiryan@volo.global>
2024-05-31 22:58:42 +0400

    trying s3 to work

--- app/s3.js
+++ app/s3.js
@@ -1,23 +1,21 @@
-const AWS = require("aws-sdk");
-
 const {
-    S3,
+    S3,S3Client
 } = require("@aws-sdk/client-s3");
 
 const dotenv = require("dotenv");
 
 dotenv.config();
 
-const spacesEndpoint = new URL(process.env.DO_SPACES_ENDPOINT);
+// const spacesEndpoint = new URL(process.env.DO_SPACES_ENDPOINT);
 
-const s3 = new S3({
-    endpoint: spacesEndpoint,
+const s3 = new S3Client({
+    endpoint: process.env.DO_SPACES_ENDPOINT,
     region: "fra1",
 
-    credentials: new AWS.Credentials({
+    credentials: {
         accessKeyId: process.env.DO_SPACES_KEY,
         secretAccessKey:  process.env.DO_SPACES_SECRET,
-    }),
+    },
 });
 
 module.exports = s3;
